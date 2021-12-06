4d43f16b34d7ec297e97b8dd0f94b665752f4e58
humhue <95610904+humhue@users.noreply.github.com>
2021-12-06 17:59:11 +0100

    Added timing of patch_all_dlgs and build_erf

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import dlg_patcher
 import erf_builder
 import os
 import sys
+import time
 
 def main():
     # python3 erf_builder.py '/home/x/Documents/DA/QUDAO/extracted_files' 'qwinn_fixpack_3_module_patched.erf'
@@ -16,14 +17,19 @@ def main():
         file_path = sys.argv[2] # qwinn_fixpack_3_module_patched.erf
     except IndexError:
         print("You did not specify a path for the .erf file")
-        sys.exit(1)
-
+        sys.exit(1)  
+    
     print("Patching .dlg files...")
+    t1 = time.time()
     dlg_patcher.patch_all_dlgs(dir_path)
-    print("Finished patching!")
+    t2 = time.time() - t1
+    print("Finished patching in " + str(t2) + "s!")
+    
     print("Now building the .erf file...")
+    t1 = time.time()
     erf_builder.build_erf(dir_path, file_path)
-    print("Finished building!")
+    t2 = time.time() - t1
+    print("Finished building in " + str(t2) + "s!")
 
 if __name__ == "__main__":
     main()
